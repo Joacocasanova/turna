d21da368d45fa09805bd212515c275430bd149ea
rocio picaza <rocio.picaza@gmail.com>
2021-02-11 21:26:35 -0300

    creating seed and rating to providers

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,8 +1,9 @@
 class Provider < ApplicationRecord
   has_many :bookings
+  has_many_attached :photos
 
-  validates :category, presence :true
-  validates :name, presence :true
-  validates :address, presence :true
-  validates :description, presence :true, length: {minimum:5}
+  validates :category, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true, length: {minimum:5}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_005711) do
+ActiveRecord::Schema.define(version: 2021_02_12_001248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_02_10_005711) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
   end
 
   create_table "users", force: :cascade do |t|
