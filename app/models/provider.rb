32bc05da1b3c8af76970ae203a8126309c67a9c9
Joaquin Casanova <joaco.c91@gmail.com>
2021-02-13 12:36:46 -0300

    added some fixes and migrations

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -6,4 +6,5 @@ class Provider < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :description, presence: true, length: {minimum:5}
+  validate :rating, presence: true, length: { minimun: 1..5 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :bookings
+  has_one_attached :avatar
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :avatar, presence: true
 end
