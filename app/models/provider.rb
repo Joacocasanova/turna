10af3d3d92d7106b0afa3b79cc218912c5766e27
tomas agnese <tagnese1985@gmail.com>
2021-02-12 23:34:04 -0300

    index front

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -2,8 +2,11 @@ class Provider < ApplicationRecord
   has_many :bookings
   has_many_attached :photos
 
-  validates :category, presence :true
-  validates :name, presence :true
-  validates :address, presence :true
-  validates :description, presence :true, length: {minimum:5}
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
+  validates :category, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true, length: {minimum:5}
 end
