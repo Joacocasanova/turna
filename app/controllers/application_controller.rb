346cf367dfabff89b2c88b529b20012191a972f6
Joaquin Casanova <joaco.c91@gmail.com>
2021-02-27 14:32:54 -0300

    changes to seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
   protect_from_forgery with: :exception
 
@@ -11,4 +12,14 @@ class ApplicationController < ActionController::Base
 
             devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :last_name, :email, :password, :current_password, :avatar, :password_confirmation)}
        end
+
+  private
+
+     def storable_location?
+          request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+     end
+
+     def store_user_location!
+          store_location_for(:user, request.fullpath)
+     end
 end
