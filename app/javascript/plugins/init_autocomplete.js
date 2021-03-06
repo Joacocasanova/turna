7e310a59f45d3e75944be655b923647019c3d64a
tomas agnese <tagnese1985@gmail.com>
2021-03-06 18:20:45 -0300

    pending providers view fixed

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -2,8 +2,13 @@ import places from 'places.js';
 
 const initAutocomplete = () => {
   const addressInput = document.getElementById('provider_address');
+  const neighborhoodInput = document.getElementById('provider_neighborhood');
+  
   if (addressInput) {
     places({ container: addressInput, countries: ['ar'] });
+  }
+  if (neighborhoodInput) {
+    places({ container: neighborhoodInput, countries: ['ar'] });
   }
 };
 
