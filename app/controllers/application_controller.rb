826534f977f8bbe0280c22fea9d17857797d2dbd
rocio picaza <rocio.picaza@gmail.com>
2021-03-13 15:31:22 -0300

    add-404-500-page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,33 @@
 class ApplicationController < ActionController::Base
-     before_action :store_user_location!, if: :storable_location?
-     before_action :authenticate_user!
-     protect_from_forgery with: :exception
-     
-     before_action :configure_permitted_parameters, if: :devise_controller?
-     
-     rescue_from ActiveRecord::RecordNotFound, with: :not_found 
-     rescue_from Exception, with: :not_found
-     rescue_from ActionController::RoutingError, with: :not_found
+  before_action :store_user_location!, if: :storable_location?
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found 
+  rescue_from Exception, with: :not_found
+  rescue_from ActionController::RoutingError, with: :not_found
+   
+  def raise_not_found
+    raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+  end
+   
+  def not_found
+    respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/404", layout: false, status: :not_found }
+      format.xml { head :not_found }
+      format.any { head :not_found }
+    end
+  end
+   
+  def error
+    respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/500", layout: false, status: :error }
+      format.xml { head :not_found }
+      format.any { head :not_found }
+    end
+  end   
 
   protected
 
@@ -37,23 +57,5 @@ class ApplicationController < ActionController::Base
 
 
 
-  def raise_not_found
-    raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
-  end
 
-  def not_found
-    respond_to do |format|
-    format.html { render file: "#{Rails.root}/public/404", layout: false, status: :not_found }
-    format.xml { head :not_found }
-    format.any { head :not_found }
-    end
-  end
-
-  def error
-    respond_to do |format|
-      format.html { render file: "#{Rails.root}/public/500", layout: false, status: :error }
-      format.xml { head :not_found }
-      format.any { head :not_found }
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,8 @@ Rails.application.routes.draw do
   post 'prepare_payment', to: 'bookings#prepare_payment'
   mount StripeEvent::Engine, at: '/stripe-webhooks'
   get '*unmatched_route', to: 'application#raise_not_found'
+
+  get "/404", :to => "errors#not_found"
+  get "/422", :to => "errors#unacceptable"
+  get "/500", :to => "errors#internal_error"
 end
