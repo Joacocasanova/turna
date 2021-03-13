17a794878e1e03aea5a3af6cc445a43befb3353a
tomas agnese <tagnese1985@gmail.com>
2021-03-13 00:17:31 -0300

    added modal in form

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -2,13 +2,25 @@ import places from 'places.js';
 
 const initAutocomplete = () => {
   const addressInput = document.getElementById('provider_address');
+  const addressInput1 = document.getElementById('provider_address1');
   const neighborhoodInput = document.getElementById('provider_neighborhood');
+  const neighborhoodInput1 = document.getElementById('provider_neighborhood1');
   
   if (addressInput) {
     places({ container: addressInput, countries: ['ar'] });
   }
+
+  if (addressInput1) {
+    places({ container: addressInput1, countries: ['ar'] });
+  }
+
+
   if (neighborhoodInput) {
     places({ container: neighborhoodInput, countries: ['ar'] });
+  }
+
+  if (neighborhoodInput1) {
+    places({ container: neighborhoodInput1, countries: ['ar'] });
   }
 };
 
